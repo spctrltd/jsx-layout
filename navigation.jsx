4e69fd5d759b8d9b3e361ebcb30d22c71dc1ad6b
Charles, SPCTR LTD <info@spctr.ltd>
2024-03-05 11:10:45 +0200

    update navigation

--- navigation.jsx
+++ navigation.jsx
@@ -1,14 +1,15 @@
 export default props => {
-  const {links = [], header, footer, ...remainingProps} = props
+  const {links, header, footer, ...remainingProps} = props
   const {props: headerProps, children: headerChildren} = header
   const {props: footerProps, children: footerChildren} = footer
+  const {data = [], props: linkProps} = links
   return (
     <nav {...remainingProps}>
       <div {...headerProps}>{headerChildren}</div>
       <div>
         <ul>
-          {links.map(link => (
-            <li>{link}</li>
+          {data.map(link => (
+            <li {...linkProps}>{link}</li>
           ))}
         </ul>
       </div>
